bc7fd80af35518a538e2229f4ad25ab705b6a572
Elliot Saba <staticfloat@gmail.com>
2018-08-21 08:44:26 -0700

    Update FLAC builder recipe

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,5 +1,8 @@
 using BinaryBuilder
 
+name = "FLAC"
+version = v"1.3.2"
+
 # Collection of sources required to build Ipopt
 sources = [
     "https://downloads.xiph.org/releases/flac/flac-1.3.2.tar.xz" =>
@@ -8,7 +11,7 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/flac-1.3.2
+cd $WORKSPACE/srcdir/flac-*
 
 # For programs with older configure steps, we often need to update and regenerate them
 update_configure_scripts
@@ -22,31 +25,11 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line.
-platforms = [
-    # Windows
-    Windows(:i686),
-    Windows(:x86_64),
-
-    # Hello linux my old friend
-    Linux(:i686, :glibc),
-    Linux(:x86_64, :glibc),
-    Linux(:aarch64, :glibc),
-    Linux(:armv7l, :glibc),
-    Linux(:powerpc64le, :glibc),
-
-    # Add some musl love
-    Linux(:i686, :musl),
-    Linux(:x86_64, :musl),
-
-    # The BSD's (FreeBSD put on hold until we fix the -fPIC debacle)
-    #FreeBSD(:x86_64),
-    MacOS(),
-]
-
+platforms = supported_platforms()
 
 dependencies = [
     # We want libogg to power up our FLAC
-    "https://github.com/staticfloat/OggBuilder/releases/download/v1.3.3-3/build.jl",
+    "https://github.com/staticfloat/OggBuilder/releases/download/v1.3.3-4/build.jl",
 ]
 
 # The products that we will ensure are always built
@@ -54,4 +37,4 @@ products = prefix -> [
     LibraryProduct(prefix, "libFLAC", :libflac),
 ]
 
-build_tarballs(ARGS, "FLAC", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
